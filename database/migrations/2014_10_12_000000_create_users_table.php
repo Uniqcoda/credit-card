21c08565ac68608b26a9c01bf15f6181c0ac18c9
Mary Etokwudo <etokwudomary@gmail.com>
2019-10-21 15:17:06 +0100

    set up boilerplate

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,53 @@
+<?php
+// trying to create enum for user type
+namespace abenevaut\Infrastructure\Interfaces\Domain\Users\Users;
+interface UserRolesInterface
+{
+   const ROLE_ADMIN = 'admin';
+   const ROLE_CUSTOMER = 'customer';
+   const ROLES = [
+      self::ROLE_ADMIN,
+      self::ROLE_CUSTOMER,
+   ];
+}
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+// use
+use abenevaut\Infrastructure\Interfaces\Domain\Users\Users\UserRolesInterface;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone', 12);
+            $table->date('dob');
+            $table->enum('role', UserRolesInterface::ROLES)->default(UserRolesInterface::ROLE_CUSTOMER)->index();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
